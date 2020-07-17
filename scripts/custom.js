9a6c2ad3a76da8c836cd86a65fe62152f88337f7
Justin Golden <justingolden21@gmail.com>
2020-07-17 15:01:13 -0700

    clean js code

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,22 +1,10 @@
-$(document).ready(() => {
-  if ("addEventListener" in document) {
-    document.addEventListener(
-      "DOMContentLoaded",
-      function () {
-        FastClick.attach(document.body);
-      },
-      false
-    );
-  }
+$( () => {
 
-  // OR if your using jQuery
-  $(function () {
-    FastClick.attach(document.body);
+  FastClick.attach(document.body);
+
+  $('#nav-dropdown-btn').click( () => {
+    $('.header').toggleClass('nav-open').toggleClass('nav-closed');
+    $('#nav-dropdown-btn').toggleClass('flipped');
   });
 
-  $("#nav-dropdown-btn").click(() => {
-    $(".header").toggleClass("nav-open").toggleClass("nav-closed");
-    $("#nav-dropdown-btn").toggleClass("flipped");
-  });
-  
 });
